2f59b9f871bef3217241e9084ba4d70934ccc298
Andrija <109519980+andrijaweb@users.noreply.github.com>
2025-06-14 22:02:18 +0200

    Added Funkcija class declaration and implementation

---
+++ assignment-7/Funkcija.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Funkcija
+{
+public:
+	virtual double operator()(double x) const = 0;
+	virtual void citaj(istream& in) = 0;
+	virtual void pisi(ostream& out) const = 0;
+
+	virtual ~Funkcija() {}
+};
+
